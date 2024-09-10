b9e4227c83158952c8ed82e8f3e7ca145465285d
Alessandro Reis <alessandrodreis23@gmail.com>
2024-09-10 13:26:54 -0300

    add empty home screen

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,3 +1,23 @@
+import { Plus } from 'lucide-react'
+import logo from './assets/logo-in-orbit.svg'
+import letsStart from './assets/lets-start-illustration.svg'
+
 export function App() {
-  return <h1 className="bg-black text-white text-3xl">Hello World</h1>
+  return (
+    <div className="h-screen flex flex-col items-center justify-center gap-8">
+      <img src={logo} alt="in.orbit" />
+      <img src={letsStart} alt="lets start" />
+      <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
+        Você ainda não cadastrou nenhuma meta, que tal cadastrar um agora mesmo?
+      </p>
+
+      <button
+        type="button"
+        className="px-4 py-2.5 rounded-lg bg-violet-500 text-violet-50 flex items-center gap-2 text-sm font-medium tracking-tight hover:bg-violet-600"
+      >
+        <Plus className="size-4" />
+        Cadastrar meta
+      </button>
+    </div>
+  )
 }
