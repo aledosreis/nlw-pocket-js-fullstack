e1999bfda82054155a2ab38392b3ce8363b14865
Alessandro Reis <alessandrodreis23@gmail.com>
2024-09-11 10:38:56 -0300

    add fetch request to complete a goal and invalidate old requests cached

--- web/src/components/pending-goals.tsx
+++ web/src/components/pending-goals.tsx
@@ -1,9 +1,12 @@
 import { Plus } from 'lucide-react'
-import { useQuery } from '@tanstack/react-query'
+import { useQuery, useQueryClient } from '@tanstack/react-query'
 import { OutlineButton } from './ui/outline-button'
 import { getPendingGoals } from '../http/get-pending-goals'
+import { createGoalCompletion } from '../http/create-goal-completion'
 
 export function PendingGoals() {
+  const queryClient = useQueryClient()
+
   const { data } = useQuery({
     queryKey: ['pending-goals'],
     queryFn: getPendingGoals,
@@ -14,6 +17,13 @@ export function PendingGoals() {
     return null
   }
 
+  async function handleCompleteGoal(goalId: string) {
+    await createGoalCompletion(goalId)
+
+    queryClient.invalidateQueries({ queryKey: ['summary'] })
+    queryClient.invalidateQueries({ queryKey: ['pending-goals'] })
+  }
+
   return (
     <div className="flex flex-wrap gap-3">
       {data.map(goal => {
@@ -21,6 +31,7 @@ export function PendingGoals() {
           <OutlineButton
             key={goal.id}
             disabled={goal.completionCount >= goal.desiredWeeklyFrequency}
+            onClick={() => handleCompleteGoal(goal.id)}
           >
             <Plus className="size-4 text-zinc-600" />
             {goal.title}
